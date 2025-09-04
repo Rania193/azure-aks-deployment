c69d2178aa4b7b225fd7923d80ee9f836926d302
Rania <rania.abdullateef@vodafone.com>
2025-09-04 22:39:01 +0300

    add prometheus flask exporter

--- application/run.py
+++ application/run.py
@@ -1,5 +1,7 @@
 from app.main import app
 import os
+from prometheus_flask_exporter import PrometheusMetrics
+metrics = PrometheusMetrics(app)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
